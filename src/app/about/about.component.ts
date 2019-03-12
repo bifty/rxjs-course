addff8a634a442647a7d4a260234bf172ee68c64
Sebastian Geiger <bastigeiger@gmail.com>
2019-03-12 21:42:04 +0100

    1.7 - 3 Core RxJs Concepts - Errors, Completion and Subscriptions

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, fromEvent, timer } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,20 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const interval$ = timer(3000, 1000);
+
+    const sub = interval$.subscribe( val => {
+      console.log('stream 1 => ' + val);
+    });
+
+    setTimeout(() => sub.unsubscribe(), 5000);
+
+    const click$ = fromEvent(document, 'click');
+    click$.subscribe(
+      evt => console.log(evt),
+      err => console.log(err),
+      () => console.log('completed')
+    );
   }
 
 }
