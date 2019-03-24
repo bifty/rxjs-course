514207d5f0b441e1c608a3eede48462bc2c21107
Sebastian Geiger <bastigeiger@gmail.com>
2019-03-24 15:43:07 +0100

    unsubscribe from observable to cancel http request

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,27 +1,20 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { interval, fromEvent, timer, Observable, noop, merge } from 'rxjs';
-import { createHTTPObservable } from '../common/util';
-import { map, mergeMap } from 'rxjs/operators';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { interval, fromEvent, timer, Observable, noop, merge } from "rxjs";
+import { createHTTPObservable } from "../common/util";
+import { map, mergeMap } from "rxjs/operators";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const http$ = createHTTPObservable("/api/courses");
+    const sub = http$.subscribe(console.log);
 
-    const interval1$ = interval(1000);
-    const interval2$ = interval1$.pipe(
-      map(val => val * 10)
-      );
-
-      const result$ = merge(interval1$, interval2$);
-
-      result$.subscribe(console.log);
+    setTimeout(() => sub.unsubscribe(), 0);
   }
 }
--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,21 +1,20 @@
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
 
 export function createHTTPObservable(url: string) {
-    return Observable.create(
-      observer => {
-        fetch(url)
-        .then(response => {
-          return response.json();
-        })
-        .then(body => {
-          observer.next(body);
-          observer.complete();
-        })
-        .catch(err => {
-          observer.error(err);
-        })
-        ;
-      }
-      );
-  }
-
+  return Observable.create(observer => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch(url, { signal })
+      .then(response => {
+        return response.json();
+      })
+      .then(body => {
+        observer.next(body);
+        observer.complete();
+      })
+      .catch(err => {
+        observer.error(err);
+      });
+    return () => controller.abort();
+  });
+}
