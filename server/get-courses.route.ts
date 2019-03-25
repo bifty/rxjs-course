e3292a46f952c839a7e811851b8caab64a83e9d7
Sebastian Geiger <bastigeiger@gmail.com>
2019-03-25 16:53:21 +0100

    startWith operator for setting default values

--- server/get-courses.route.ts
+++ server/get-courses.route.ts
@@ -4,16 +4,17 @@ import { COURSES } from "./db-data";
 // dont forget to restart server after changes ...
 
 export function getAllCourses(req: Request, res: Response) {
-  const error = Math.random() >= 0.5;
+  /*
+    const error = Math.random() >= 0.5;
 
   if (error) {
     console.log("ERROR loading courses!");
     res.status(500).json({ message: "random error occurred." });
-  } else {
-    setTimeout(() => {
-      res.status(200).json({ payload: Object.values(COURSES) });
-    }, 200);
-  }
+  } else {*/
+  setTimeout(() => {
+    res.status(200).json({ payload: Object.values(COURSES) });
+  }, 200);
+  // }
 }
 
 export function getCourseById(req: Request, res: Response) {
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -44,18 +44,12 @@ export class CourseComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const searchLessons$ = fromEvent<any>(
-      this.input.nativeElement,
-      "keyup"
-    ).pipe(
+    this.lessons$ = fromEvent<any>(this.input.nativeElement, "keyup").pipe(
       map(event => event.target.value),
       debounceTime(400),
       distinctUntilChanged(),
       switchMap(search => this.loadLessons(search))
     );
-    const initialLessons$ = this.loadLessons();
-
-    this.lessons$ = concat<Lesson[]>(initialLessons$, searchLessons$);
   }
 
   loadLessons(search = ""): Observable<Lesson[]> {
